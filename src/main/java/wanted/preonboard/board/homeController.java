165afd31a9a68fec156aa3c54ba3085a868bb03f
wnstn819 <wnstn819@naver.com>
2023-09-18 03:27:29 +0900

    프로젝트 CI/CD 복기 - 정리용 - github Actions을 통한 CI/CD 다시 적용하기 - 적용 후 정리 용도

---
+++ src/main/java/wanted/preonboard/board/homeController.java
@@ -0,0 +1,19 @@
+package wanted.preonboard.board;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class homeController {
+
+    @Value("${spring.datasource.username}")
+    String name;
+
+    @GetMapping("/")
+    public String test(){
+        System.out.println("Test");
+        return "Hello CI/CD : " + name;
+    }
+
+}
